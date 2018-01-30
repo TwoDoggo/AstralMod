fa0692e31f4ab8a8d414208b2d0647c7807a9fb1
Victor Tran <vicr12345@gmail.com>
2018-01-30 11:42:33 +1100

    Update WorkFlow Module

--- plugins/wf.js
+++ plugins/wf.js
@@ -19,18 +19,36 @@
  * *************************************/
 
 const Discord = require('discord.js');
+const staffChannelID = "407659053473333269";
 var client;
 var consts;
 
 var currentWarnings = {};
 
 function processCommand(message, isMod, command) {
-    if (settings[message.guild.id] == null) {
-        settings[message.guild.id] = {};
+    if (settings.guilds[message.guild.id] == null) {
+        settings.guilds[message.guild.id] = {};
     }
 
-    if (settings[message.guild.id].roles == null) {
-        settings[message.guild.id].roles = [];
+    if (settings.guilds[message.guild.id].roles == null) {
+        settings.guilds[message.guild.id].roles = [];
+    }
+
+    if (settings.guilds[message.guild.id].prizes == null) {
+        settings.guilds[message.guild.id].prizes = [];
+    }
+
+
+    if (settings.guilds[message.guild.id].users == null) {
+        settings.guilds[message.guild.id].users = [];
+    }
+
+    if (settings.guilds[message.guild.id].users[message.author.id] == null) {
+        settings.guilds[message.guild.id].users[message.author.id] = {};
+    }
+
+    if (settings.guilds[message.guild.id].users[message.author.id].points == null) {
+        settings.guilds[message.guild.id].users[message.author.id].points = 0;
     }
 
     if (isMod) {
@@ -48,21 +66,91 @@ function processCommand(message, isMod, command) {
             if (selectedRole == null) {
                 message.reply("Couldn't toggle that role");
             } else {
-                if (settings[message.guild.id].roles.includes(selectedRole.id)) {
-                    settings[message.guild.id].roles.splice(settings[message.guild.id].roles.indexOf(selectedRole.id), 1);
+                if (settings.guilds[message.guild.id].roles.includes(selectedRole.id)) {
+                    settings.guilds[message.guild.id].roles.splice(settings.guilds[message.guild.id].roles.indexOf(selectedRole.id), 1);
                     message.reply(selectedRole.name + " has been disabled for self-role.");
                 } else {
-                    settings[message.guild.id].roles.push(selectedRole.id);
+                    settings.guilds[message.guild.id].roles.push(selectedRole.id);
                     message.reply(selectedRole.name + " has been enabled for self-role.");
                 }
+            }
+        } else if (command.startsWith("addprize ")) {
+            let prizeArgs = command.substr(9);
+            let prizePrice = prizeArgs.substr(0, prizeArgs.indexOf(" "));
+            let prizeName = prizeArgs.substr(prizeArgs.indexOf(" ") + 1);
+
+            if (parseInt(prizePrice) != prizePrice) {
+                message.reply("Invalid Price");
+                return;
+            }
+            prizePrice = parseInt(prizePrice);
+
+            for (let key in settings.guilds[message.guild.id].prizes) {
+                let prize = settings.guilds[message.guild.id].prizes[key];
+                if (prize.name.toLowerCase() == prizeName.toLowerCase()) {
+                    message.reply(prizeName + " already exists");
+                    return;
+                }
+            }
+
+            settings.guilds[message.guild.id].prizes.push({
+                name: prizeName,
+                value: prizePrice
+            });
+
+            message.reply(prizeName + " was added with a value of " + prizePrice + " points.");
+        } else if (command.startsWith("rmprize ")) {
+            let prizeName = command.substr(8);
+
+            for (let key in settings.guilds[message.guild.id].prizes) {
+                let prize = settings.guilds[message.guild.id].prizes[key];
+                if (prize.name.toLowerCase() == prizeName.toLowerCase()) {
+                    settings.guilds[message.guild.id].prizes.splice(key, 1);
+                    message.reply(prizeName + " removed");
+                    return;
+                }
+            }
+
+            message.reply(prizeName + " doesn't exist.");
+        } else if (command.startsWith("addpoints ")) {
+            let prizeArgs = command.substr(10);
+            let prizePrice = prizeArgs.substr(0, prizeArgs.indexOf(" "));
+            let user = prizeArgs.substr(prizeArgs.indexOf(" ") + 1);
+
+            if (parseInt(prizePrice) != prizePrice) {
+                message.reply("Invalid Price");
+                return;
+            }
+            prizePrice = parseInt(prizePrice);
+
+            var users = parseUser(user, message.guild);
+            if (users.length > 0) {
+                let user = null;
+
+                //Filter out members
+                for (var i = 0; i < users.length; i++) {
+                    if (message.guild.members.has(users[i].id)) {
+                        user = users[i].id;
+                        i = users.length;
+                    }
+                }
+
+                if (user == null) {
+                    throw new CommandError("No user found with that name on this server");
+                } else {
+                    message.channel.send("**" + client.users.get(user).tag + "**: " + settings.guilds[message.guild.id].users[user].points + " + " + prizePrice + " = " + (settings.guilds[message.guild.id].users[user].points + prizePrice));
+                    settings.guilds[message.guild.id].users[user].points += prizePrice;
+                }
+            } else {
+                throw new CommandError("No user found with that name");
             }
         }
     }
 
     if (command.startsWith("addrole ")) {
         let desiredRoleName = command.substr(8);
-        for (key in settings[message.guild.id].roles) {
-            let roleId = settings[message.guild.id].roles[key];
+        for (key in settings.guilds[message.guild.id].roles) {
+            let roleId = settings.guilds[message.guild.id].roles[key];
             let role = message.guild.roles.get(roleId);
             if (role.name.toLowerCase().indexOf(desiredRoleName) != -1) {
                 //Add role
@@ -79,8 +167,8 @@ function processCommand(message, isMod, command) {
         message.reply("Couldn't find a role to add you to.");
     } else if (command.startsWith("rmrole ")) {
         let desiredRoleName = command.substr(7);
-        for (key in settings[message.guild.id].roles) {
-            let roleId = settings[message.guild.id].roles[key];
+        for (key in settings.guilds[message.guild.id].roles) {
+            let roleId = settings.guilds[message.guild.id].roles[key];
             let role = message.guild.roles.get(roleId);
             if (role.name.toLowerCase().indexOf(desiredRoleName) != -1) {
                 //Remove role
@@ -95,6 +183,41 @@ function processCommand(message, isMod, command) {
         }
 
         message.reply("Couldn't find a role to remove you from.");
+    } else if (command.startsWith("prizes")) {
+        let reply = "```";
+
+        for (let key in settings.guilds[message.guild.id].prizes) {
+            let prize = settings.guilds[message.guild.id].prizes[key];
+            reply += prize.name + " - " + prize.value + " points"
+        }
+
+        reply += "```";
+
+        message.channel.send(reply);
+    } else if (command.startsWith("claim ")) {
+        let prizeName = command.substr(6);
+        
+        for (let key in settings.guilds[message.guild.id].prizes) {
+            let prize = settings.guilds[message.guild.id].prizes[key];
+            if (prize.name.toLowerCase() == prizeName.toLowerCase()) {
+                let points = settings.guilds[message.guild.id].users[message.author.id].points;
+
+                if (prize.value > points) {
+                    message.reply("You don't have enough points to redeem that prize");
+                } else {
+                    points -= prize.value;
+                    client.channels.get(staffChannelID).send(message.author.tag + " claimed " + prize.name);
+                    message.reply("You have claimed " + prize.name + " for " + prize.value + " points");
+                }
+
+                settings.guilds[message.guild.id].users[message.author.id].points = points;
+                return;
+            }
+        }
+
+        message.reply("Couldn't find the prize you wanted to claim");
+    } else if (command.startsWith("bal")) {
+        message.reply("You have " + settings.guilds[message.guild.id].users[message.author.id].points + " points");
     }
 }
 
@@ -113,7 +236,10 @@ module.exports = {
         general: {
             commands: [
                 "addrole",
-                "rmrole"
+                "rmrole",
+                "prizes",
+                "bal",
+                "claim"
             ],
             modCommands: [
                 "togglerole"
@@ -141,6 +267,22 @@ module.exports = {
                 help.usageText = prefix + "rmrole [role]";
                 help.helpText = "Remove yourself from a role.";
                 help.param1 = "The role to remove yourself from";
+                break;
+            case "claim":
+                help.title = prefix + "claim";
+                help.usageText = prefix + "claim [prize]";
+                help.helpText = "Claim a prize.";
+                help.param1 = "The prize you wish to claim";
+                break;
+            case "prizes":
+                help.title = prefix + "prizes";
+                help.usageText = prefix + "prizes";
+                help.helpText = "Show available prizes.";
+                break;
+            case "bal":
+                help.title = prefix + "bal";
+                help.usageText = prefix + "bal";
+                help.helpText = "Show your balance.";
                 break;
         }
 
