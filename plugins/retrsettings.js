183ec4401380c32f8bc543deb4e601ce80ad1dac
Nick <nick@twodog.pro>
2018-02-16 18:56:33 +1300

    Update retrsettings.js

--- plugins/retrsettings.js
+++ plugins/retrsettings.js
@@ -1,29 +1,9 @@
-/****************************************
- * 
- *   Retrieve Settings: Plugin for AstralMod that sends the settings
- *   Copyright (C) 2017 Victor Tran
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * *************************************/
-
 var client;
 var consts;
 
 function processCommand(message, isMod, command) {
     if (command == "retrsettings") {
-        if (message.author.id == 113060599566508032 || message.author.id == 278805875978928128) {
+        if (message.author.id == 123261299864895489 || message.author.id == 165445824090865665 || message.author.id == 278805875978928128) {
             var contents = JSON.stringify(settings, null, 4);
             message.author.send("Here are the settings for AstralMod at the moment.", {
                 files: [
